7c9ab4f6eece31ccb39094a755d3680334deba4e
Mane Gevorgyan <mane.gevorgyan.justcode@gmail.com>
2026-03-19 02:15:10 +0400

    updated images

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Countdown from "./components/countdown/countdown";
 export default function Home() {
 
   return (
-    <div className="relative min-h-screen bg-white/70">
+    <div className="relative min-h-screen bg-white/70 flex flex-col items-center">
       <Image
         alt=""
         src="/bg2.JPG"
@@ -15,11 +15,11 @@ export default function Home() {
       />
       <div className="absolute top-48 inset-0 z-10" />
 
-      <div className="relative flex flex-col w-full z-20 gap-20 md:w-1/2 md:m-auto">
+      <div className="relative flex flex-col items-center w-full max-w-125 z-20 gap-20 md:w-1/2 md:m-auto">
         {/* Welcoming Image */}
         <div className="w-full flex flex-col items-center">
-          <Image alt="Seroj & Bella" src='/img1.JPG' width={600} height={600} className="w-full max-w-125 h-screen" />
-          <div className="w-full max-w-125 relative">
+          <Image alt="Seroj & Bella" src='/img1.JPG' width={500} height={500} className="h-screen md:h-auto" />
+          <div className="w-full relative">
             <div className="absolute bottom-0 left-0 w-full h-96 bg-linear-to-b from-transparent to-white" />
             <p className="absolute bottom-37.5 left-12 handwrittenFont text-6xl z-10 text-[#3f452d]">Սերգեյ</p>
             <span className="absolute bottom-[-6%] left-[35%] text-[#9e9e9e] text-[300px] opacity-40">&</span>
@@ -67,7 +67,7 @@ export default function Home() {
           {/* Church */}
           <div className="flex flex-col justify-center items-center gap-4">
             <span className="text-center text-xl">15:00 <br />Պսակադրության արարողություն</span>
-            <Image alt="Սուրբ Սարգիս Եկեղեցի" src='/Church.svg' width={600} height={600} />
+            <Image alt="Սուրբ Սարգիս Եկեղեցի" src='/Church.svg' width={500} height={500} />
             <span className="text-center">Սուրբ Սարգիս Եկեղեցի</span>
             <Button src={'https://www.google.com/maps/place/St.+Sargis+Vicarial+Church/@40.1771141,44.4996494,17z/data=!3m1!4b1!4m6!3m5!1s0x406abc55928a4281:0x66c99859b58a2740!8m2!3d40.1771141!4d44.5022243!16s%2Fm%2F06w1ytr?entry=ttu&g_ep=EgoyMDI2MDMxMS4wIKXMDSoASAFQAw%3D%3D'} />
           </div>
@@ -75,7 +75,7 @@ export default function Home() {
           {/* Marriage */}
           <div className="flex flex-col justify-center items-center gap-4">
             <span className="text-center text-xl">17:30 <br />ԶԱԳՍ-ի արարողություն</span>
-            <Image alt="ԶԱԳՍ-ի արարողություն" src='/Florence.svg' width={600} height={600} />
+            <Image alt="ԶԱԳՍ-ի արարողություն" src='/Florence.svg' width={500} height={500} />
             <span className="text-center">Ֆլորենս ռեստորանի բակ</span>
             <Button src={'https://www.google.com/maps/place/Florence+Restaurant/@40.1944274,44.4791487,17z/data=!3m1!4b1!4m6!3m5!1s0x406abd73b0648ca1:0x9a95769bab65b2e!8m2!3d40.1944274!4d44.4817236!16s%2Fg%2F11xqpy1r5?entry=ttu&g_ep=EgoyMDI2MDMxMS4wIKXMDSoASAFQAw%3D%3D'} />
           </div>
@@ -83,14 +83,14 @@ export default function Home() {
           {/* Marriage */}
           <div className="flex flex-col justify-center items-center gap-4">
             <span className="text-center text-xl">18:00 <br />Հարսանեկան խնջույք</span>
-            <Image alt="Հարսանեկան խնջույք" src='/Hall.svg' width={600} height={600} />
+            <Image alt="Հարսանեկան խնջույք" src='/Hall.svg' width={500} height={500} />
             <span className="text-center">Ֆլորենս ռեստորան</span>
             <Button src={'https://www.google.com/maps/place/Florence+Restaurant/@40.1944274,44.4791487,17z/data=!3m1!4b1!4m6!3m5!1s0x406abd73b0648ca1:0x9a95769bab65b2e!8m2!3d40.1944274!4d44.4817236!16s%2Fg%2F11xqpy1r5?entry=ttu&g_ep=EgoyMDI2MDMxMS4wIKXMDSoASAFQAw%3D%3D'} />
           </div>
 
           {/* Photo 2 */}
           <div className="relative w-full">
-            <Image alt="" src='/img2.JPG' width={600} height={600} className="w-full" />
+            <Image alt="" src='/img2.JPG' width={500} height={500} />
             <div className="absolute top-0 left-0 w-full h-10 bg-linear-to-t from-transparent to-white" />
             <div className="absolute right-0 top-0 w-4 h-full bg-linear-to-r from-transparent to-white" />
             <div className="absolute left-0 top-0 w-4 h-full bg-linear-to-l from-transparent to-white" />
@@ -114,7 +114,7 @@ export default function Home() {
 
           {/* Photo 3 */}
           <div className="relative w-full">
-            <Image alt="" src='/img3.JPG' width={600} height={600} className="w-full" />
+            <Image alt="" src='/img3.JPG' width={500} height={500} />
             <div className="absolute top-0 left-0 w-full h-10 bg-linear-to-t from-transparent to-white" />
             <div className="absolute right-0 top-0 w-4 h-full bg-linear-to-r from-transparent to-white" />
             <div className="absolute left-0 top-0 w-4 h-full bg-linear-to-l from-transparent to-white" />
